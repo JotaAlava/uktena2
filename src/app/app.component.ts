b207cfd271f8631b42843ce42a6a5a8f66cfcc0c
Jose Alava <jalava85@gmail.com>
2019-05-14 22:20:16 -0400

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import Auth0Cordova from '@auth0/cordova';
+import Auth0 from 'auth0-js';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -19,8 +22,15 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      // Add this function
+      (<any>window).handleOpenURL = (url) => {
+        Auth0Cordova.onRedirectUri(url);
+      };
     });
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
+  providers: [AuthService]
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(public auth: AuthService) {
+  }
+}
